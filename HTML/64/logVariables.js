3e0e700e7caeb017e87505f43295b7f828bbc61c
mordechaibesser <mordechaitzvi23@gmail.com>
2024-07-15 17:51:46 -0400

    HW64 JS

---
+++ HTML/64/logVariables.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log("Global Name: " + myName);
+console.log("Global Email: " + email);
---
+++ HTML/64/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var myName = "Mordechai Besser";
+var email = "mordechaitzvi23@gmail.com";
+
+var userName = prompt("What's your name?");
+console.log("User Name:", userName);
+alert("User Name: " + userName);
