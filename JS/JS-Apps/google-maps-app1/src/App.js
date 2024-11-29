5994a9ce7a4723f6cf80ca92f47447d97c1d35d9
mordechaibesser <mordechaitzvi23@gmail.com>
2024-11-29 02:10:44 -0500

    Update App.js

--- JS/JS-Apps/google-maps-app1/src/App.js
+++ JS/JS-Apps/google-maps-app1/src/App.js
@@ -1,12 +1,12 @@
 // src/App.js
 import React from "react";
-import Map from "./components/Map"; // Import Map component
+import Map from "./components/Map"; 
 
 const App = () => {
   return (
     <div style={{ textAlign: "center", fontFamily: "Arial, sans-serif" }}>
       <h1>My Cool Google Maps App</h1>
-      <Map /> {/* Render Map component here */}
+      <Map /> 
     </div>
   );
 };
